d341669abda00227d9856cfdd67dcf3c8abc407a
monbira <monikabiras@gmail.com>
2023-03-12 23:43:18 +0100

    .

--- src/main/java/cz/czechitas/ukol3/HlavniProgram.java
+++ src/main/java/cz/czechitas/ukol3/HlavniProgram.java
@@ -6,8 +6,37 @@ package cz.czechitas.ukol3;
 public class HlavniProgram {
 
     public static void main(String... args) {
-        //TODO tady bude váš kód - vytvoření instance třídy Pocitac, zapnutí, vpynutí, výpis hodnot.
+
+            Pocitac monikyPocitac;
+            monikyPocitac = new Pocitac();
+            System.out.println(monikyPocitac.toString());
+
+
+            Procesor monikyProcesor = new Procesor();
+            monikyProcesor.setRychlost(2_000_000_000_000L);
+            monikyProcesor.setVyrobce("Intel");
+
+            Pamet monikyPamet = new Pamet();
+            monikyPamet.setKapacita(3_450_000_000L);
+
+            Disk monikyDisk = new Disk();
+            monikyDisk.setKapacita(450_000_000_000L);
+
+            monikyPocitac.setCpu(monikyProcesor);
+            monikyPocitac.setRam(monikyPamet);
+            monikyPocitac.setPevnyDisk(monikyDisk);
+
+            System.out.println(monikyPocitac.toString());
+
+            monikyPocitac.zapniSe();
+
+            System.out.println(monikyPocitac.toString());
+
+            monikyPocitac.vypniSe();
+
+        }
+
         System.out.println("Program spuštěn.");
     }
 
-}
+
