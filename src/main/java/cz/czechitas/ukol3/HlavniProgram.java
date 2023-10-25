363b5d94a84f12a4ead967aa1348abfd83f606f5
monbira <monikabiras@gmail.com>
2023-10-25 20:13:11 +0200

    pocitacMoniky

--- src/main/java/cz/czechitas/ukol3/HlavniProgram.java
+++ src/main/java/cz/czechitas/ukol3/HlavniProgram.java
@@ -13,30 +13,50 @@ public class HlavniProgram {
 
 
             Procesor monikyProcesor = new Procesor();
-            monikyProcesor.setRychlost(2_000_000_000_000L);
+            monikyProcesor.setRychlost(2_800_000_000L);
             monikyProcesor.setVyrobce("Intel");
 
             Pamet monikyPamet = new Pamet();
-            monikyPamet.setKapacita(3_450_000_000L);
+            monikyPamet.setKapacita(16_000_000_000L);
 
-            Disk monikyDisk = new Disk();
-            monikyDisk.setKapacita(450_000_000_000L);
+            Disk monikyDisk = new Disk(1_000_000_000_000L, 291_000_000_000L, "jedna - pevný disk");
+            //monikyDisk.setKapacita(450_000_000_000L);
+
+            Disk extDisk = new Disk(3_000_000_000_000L, "dvě - externí disk");
 
             monikyPocitac.setCpu(monikyProcesor);
             monikyPocitac.setRam(monikyPamet);
             monikyPocitac.setPevnyDisk(monikyDisk);
+            monikyPocitac.setExterniDisk(extDisk);
 
             System.out.println(monikyPocitac.toString());
 
+            monikyPocitac.zapniSe();
             monikyPocitac.zapniSe();
 
             System.out.println(monikyPocitac.toString());
 
             monikyPocitac.vypniSe();
 
-        }
+            monikyPocitac.zapniSe();
 
-        System.out.println("Program spuštěn.");
+            monikyPocitac.vytvorSouborOVelikosti(333_000L);
+
+
+            monikyPocitac.vypniSe();
+
+            monikyPocitac.vytvorSouborOVelikosti(200_000L);
+
+
+            monikyPocitac.zapniSe();
+
+            monikyPocitac.vytvorSouborOVelikosti(300_000L);
+
+            monikyPocitac.vymazSouboryOVelikosti(300_000L);
+
+
+            monikyPocitac.vytvorSouborOVelikosti(2_500_000_000_000L);
+            monikyPocitac.vymazSouboryOVelikosti(2_500_000_000_000L);
     }
 
-
+}
---
+++ src/main/java/cz/czechitas/ukol3/Procesor.java
@@ -0,0 +1,27 @@
+package cz.czechitas.ukol3;
+
+public class Procesor {
+    private String vyrobce;
+    private long rychlost;
+
+    public String getVyrobce() {
+        return vyrobce;
+    }
+
+    public void setVyrobce(String vyrobce) {
+        this.vyrobce = vyrobce;
+    }
+
+    public long getRychlost() {
+        return rychlost;
+    }
+
+    public void setRychlost(long rychlost) {
+        this.rychlost = rychlost;
+    }
+
+    @Override
+    public String toString() {
+        return "Procesor: " + vyrobce + " " + rychlost + " Hz.";
+    }
+}
